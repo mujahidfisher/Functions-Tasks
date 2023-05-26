5aa50bded129f15af7de71379816e80a64cc35c1
mujahidfisher <fishermujahid@gmail.com>
2023-05-26 11:38:04 +0200

    just test

---
+++ script/code.js
@@ -0,0 +1,22 @@
+function greet(para1) {
+    para1 = "Hello"
+}
+
+console.log(para1);
+
+// function multiply(numb1, numb2) {
+//     numb1 = 5,
+//     numb2 = 4
+// }
+
+// let result = numb1 * numb2
+
+// console.log(result);
+
+// function tableOfFive(water) {
+//     water = 1 * 5
+// }
+
+// let water = 1
+
+// console.log(water);
